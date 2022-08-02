ec4dd0975afb2a3948165ad8d0480f5c3aea86f1
sadie100 <sadie1001@naver.com>
2022-08-03 00:16:13 +0900

    20220803

---
+++ nodejs/test.js
@@ -0,0 +1,15 @@
+const fs=require('fs');
+const input = fs.readFileSync('test.txt').toString().split("\n");
+
+const num = input.shift();
+let arr = input.map(d=>d.split(" ").map(a=>Number(a)));
+arr = arr.sort((a,b)=>{if(a[1]!==b[1]){return a[1]-b[1]}else{return  a[0]-b[0]}})
+let result=0, recentEnd=0;
+arr.forEach(([start, end],idx)=>{
+    if(recentEnd>start){
+        return;
+    }
+    result++;
+    recentEnd=end;
+})
+console.log(result)
