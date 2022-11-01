f74a1e634f43b52a478027a5c9c1a178af6a5291
김세희 <sadie1001@naver.com>
2022-11-02 00:07:02 +0900

    외판원 문제 2 완료

--- python/10971.py
+++ python/10971.py
@@ -1,23 +1,29 @@
 n = int(input())
+
 route = []
 for i in range(n):
     route.append(list(map(int,input().split())))
 
 least_cost=0
 
-def dfs (now, visited, cost):
-    visited[now] = True
-    to = [d for idx, d in enumerate(route[now]) if not visited[idx]]
-    print(to)
-    if(len(to)==0):
-        least_cost = min(cost+route[now][now],least_cost)
-        return
+def dfs (now, visited, cost, startIdx):
+    global least_cost
+    if(least_cost>0 and cost>least_cost): return
+    to = [idx for idx, d in enumerate(route[now]) if not visited[idx] and d!=0]
     
-    for next in [togo for togo in visited if togo==False]:
-        dfs(next, visited, cost+route[now][next])
+    if(len(to)==0):
+        if all(flag==True for flag in visited) and route[now][startIdx]!=0:
+            least_cost = cost + route[now][startIdx] if least_cost == 0 else min(cost + route[now][startIdx], least_cost)
+        return
+
+    for next in to:
+        visited[next] = True
+        dfs(next, visited, cost+route[now][next], startIdx)
+        visited[next] = False
 
 for i in range(n):
     visited = [False] * n
-    dfs(i, visited, 0)
+    visited[i] = True
+    dfs(i, visited, 0, i)
 
 print(least_cost)
