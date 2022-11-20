0961865e91370e5ecc22a979e69e74be9c6d3c6e
김세희 <sadie1001@naver.com>
2022-11-20 23:53:25 +0900

    11.20 문제풀이

---
+++ 백준/품/silver/11047_silver4_동전 0.py
@@ -0,0 +1,19 @@
+'''
+동전 큰거부터 되는대로 채운다..
+'''
+
+n,k = map(int, input().split())
+coins = [0]*n
+count = 0
+for idx in range(n):
+    coins[idx] = int(input())
+
+coins = [coin for coin in coins if coin<=k]
+coins.reverse()
+
+for coin in coins:
+    if k>=coin:
+        count += k//coin
+        k %= coin
+
+print(count)
