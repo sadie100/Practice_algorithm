4039cc5768e48859e147155a82d63893efd351e6
김세희 <sadie1001@naver.com>
2023-03-31 21:29:57 +0900

    03.31 문제풀이

--- 프로그래머스/못품/표 병합.py
+++ 프로그래머스/못품/표 병합.py
@@ -1,29 +1,8 @@
-def merging(table, r1, c1, r2, c2):
-    if r1==r2 and c1==c2: return
-    for r, c in table[r1][c1][1]:
-        if r==r2 and c==c2: continue
-        if [r2, c2] in table[r][c][1]:continue
-        table[r][c][1].append([r2,c2])
-        table[r2][c2][1].append([r,c])
-        merging(table, r, c, r2, c2)
-        merging(table, r2, c2, r, c)
-
-def unmerging(table, r, c, notR, notC):
-    if not table[r][c][1] : return
-    removinglist = table[r][c][1]
-    if(r!=notR and c!=notC): table[r][c][0] = ''
-    table[r][c][1] = []
-    
-    for r1, c1 in removinglist:
-        unmerging(table, r1, c1, notR, notC)
-    
-
 def solution(commands):
     answer = []
-    # 테이블 값 형태 : [값, merge된 셀 list]
-    size = 51
-    table = [[['', []] for _ in range(size)] for _ in range(size)]
-    
+    merged = [(i,j) for i in range(51) for j in range(51)]
+    content = ["EMPTY" for _ in range(51) for j in range(51)]
+
     for com in commands:
         coms = com.split()
         if com.find('UPDATE')!=-1:
@@ -32,32 +11,34 @@ def solution(commands):
                 command, r, c, val = coms
                 r = int(r)
                 c = int(c)
-                table[r][c][0] = val
-                for r,c in table[r][c][1]:
-                    table[r][c][0] = val
+                x,y = merged[r][c]
+                content[x][y] = val
             else:
                 # 두 번째 경우
                 command, val1, val2 = coms
-                for i in range(size):
-                    for j in range(size):
-                        val, mergeList = table[i][j]
-                        if val==val1:
-                            table[i][j][0] = val2
+                for i in range(51):
+                    for j in range(51):
+                        if content[i][j] == val1:
+                            content[i][j] = val2
         elif com.find('UNMERGE')!=-1:
             # UNMERGE 처리
             command, r, c = coms
             r = int(r)
             c = int(c)
-            if len(table[r][c][1])>0:
-                for r1, c1 in table[r][c][1]:
-                    table[r1][c1][0] =''
-                    table[r1][c1][1] = []
-                table[r][c][1] = []
+            if merged[r][c] == (r,c):
+                # r, c인 merged를 본인 것으로 변경
+                for i in range(51):
+                    for j in range(51):
+                        if merged[i][j] == (r,c):
+                            merged[i][j] = (i,j)
+            else:
+                merged[r][c] = (r,c)
         elif com.find('MERGE')!=-1:
             command, *rest = coms
             r1, c1, r2, c2 = list(map(int, rest))
             if(r1==r2 and c1==c2): continue
             
+            merged[r1][c1] = (r2,c2)
             table[r1][c1][1].append([r2,c2])
             table[r2][c2][1].append([r1,c1])
             
@@ -76,4 +57,6 @@ def solution(commands):
                 answer.append(table[r][c][0])
             else:
                 answer.append("EMPTY")
+    return answer
+    
     return answer
