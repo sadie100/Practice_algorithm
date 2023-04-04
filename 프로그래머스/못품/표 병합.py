8301e617ba3214c0b7d4758d619c718241b45586
김세희 <sadie1001@naver.com>
2023-04-04 17:06:41 +0900

    04.04 문제풀이

--- 프로그래머스/못품/표 병합.py
+++ 프로그래머스/못품/표 병합.py
@@ -1,12 +1,12 @@
 def solution(commands):
     answer = []
-    merged = [(i,j) for i in range(51) for j in range(51)]
-    content = ["EMPTY" for _ in range(51) for j in range(51)]
-
+    merged = [[(j,i) for i in range(51)] for j in range(51)]
+    content = [["EMPTY" for _ in range(51)] for j in range(51)]
+    
     for com in commands:
         coms = com.split()
         if com.find('UPDATE')!=-1:
-            if coms[1].isnumeric():
+            if len(coms)==4:
                 # 첫 번째 경우
                 command, r, c, val = coms
                 r = int(r)
@@ -25,38 +25,39 @@ def solution(commands):
             command, r, c = coms
             r = int(r)
             c = int(c)
-            if merged[r][c] == (r,c):
-                # r, c인 merged를 본인 것으로 변경
-                for i in range(51):
-                    for j in range(51):
-                        if merged[i][j] == (r,c):
-                            merged[i][j] = (i,j)
-            else:
-                merged[r][c] = (r,c)
+            w, h = merged[r][c]
+            if merged[r][c] != (r,c):
+                written = content[w][h]
+                content[w][h] = 'EMPTY'
+                content[r][c] = written
+            for i in range(51):
+                for j in range(51):
+                    if merged[i][j] == (w,h):
+                        merged[i][j] = (i,j)
+                        if i==r and j==c: continue
+                        content[i][j] = 'EMPTY'
+            
         elif com.find('MERGE')!=-1:
             command, *rest = coms
             r1, c1, r2, c2 = list(map(int, rest))
             if(r1==r2 and c1==c2): continue
             
-            merged[r1][c1] = (r2,c2)
-            table[r1][c1][1].append([r2,c2])
-            table[r2][c2][1].append([r1,c1])
-            
-            merging(table, r1, c1, r2, c2)
-            merging(table, r2, c2, r1, c1)
-            
-            
-            if table[r1][c1][0]:
-                table[r2][c2][0] = table[r1][c1][0]
+            w1, h1 = merged[r1][c1]
+            w2, h2 = merged[r2][c2]
+            if content[w1][h1] == 'EMPTY' and content[w2][h2]!='EMPTY':
+                for i in range(51):
+                    for j in range(51):
+                        if merged[i][j] == (w1,h1):
+                            merged[i][j] = (w2,h2)
             else:
-                table[r1][c1][0] = table[r2][c2][0]
+                for i in range(51):
+                    for j in range(51):
+                        if merged[i][j] == (w2,h2):
+                            merged[i][j] = (w1,h1)
         elif com.find('PRINT')!=-1:
             command, *rest = coms
             r, c = list(map(int, rest))
-            if table[r][c][0]:
-                answer.append(table[r][c][0])
-            else:
-                answer.append("EMPTY")
-    return answer
+            w1, h1 = merged[r][c]
+            answer.append(content[w1][h1])
     
     return answer
