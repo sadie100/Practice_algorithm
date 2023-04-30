b941c5c4f2a0d0cc7c6221daf14a21756bfcd6db
김세희 <sadie1001@naver.com>
2023-04-30 17:59:33 +0900

    04.30 문제풀이

--- 백준/못품/10816_0_숫자 카드 2.py
+++ 백준/못품/10816_0_숫자 카드 2.py
@@ -1,39 +1,36 @@
+from sys import stdin
+input = stdin.readline
 
-def getNum(number):
-    minNum = -10000000
-    maxNum = 10000000
-    flag = False
-
-    while(True):
-        if minNum>maxNum : break
-        half = (minNum)+(maxNum)//2
-        if number==half:
-            flag=True
-            break
-        elif number>half:
-            maxNum = half-1
-        else:
-            minNum = half+1
-        
-    return flag
+def getNumber(start, end, num):
+    global cards, answer, dp
+    if num in dp:
+        answer += ' ' + dp[num]
+        return
+    if start>end:
+        answer += ' 0'
+        return
+    half = (start + end)//2
+    if num==cards[half]:
+        counted = str(cards[start:end+1].count(num))
+        answer += ' ' + counted
+        dp[num] = counted
+        return
+    elif num>cards[half]:
+        getNumber(half+1, end, num)
+    else:
+        getNumber(start, half-1,num)
 
 n = int(input())
 cards = list(map(int, input().split()))
 m = int(input())
 quizes = list(map(int, input().split()))
+dp = {}
 
-cntObj = dict.fromkeys(set(cards).union(set(quizes)), 0)
+cards.sort()
+answer = ''
 
-for card in cards:
-    cntObj[card] += 1
-
-answer= ''
 for quiz in quizes:
-    res = cntObj[quiz]
-    answer += ' ' + str(res)
-    # if quiz in cntObj:
-        
-    # else:
-    #     answer += ' ' + '0'
+    if quiz in dp: continue
+    getNumber(0, len(cards)-1, quiz)
 
 print(answer.strip())
