19027f524a2280afdc0d4b518e7ea5a7f9780c8a
김세희 <sadie1001@naver.com>
2023-05-17 23:25:12 +0900

    05.17 문제풀이

--- test.js
+++ test.js
@@ -1,32 +1,10 @@
-//1158 요세푸스 문제
 const fs = require("fs");
-const [N, K] = fs
+const [n, ...numbers] = fs
   .readFileSync("input.txt")
   .toString()
   .trim()
-  .split(" ")
+  .split("\n")
   .map(Number);
 
-const used = new Array(N + 1).fill(false);
-let count = 0;
-const answer = [];
-let remain = N;
-
-while (true) {
-  for (let i = 1; i <= N; i++) {
-    if (used[i]) {
-      continue;
-    }
-    count += 1;
-    if (count === K) {
-      answer.push(i);
-      used[i] = true;
-      count = 0;
-      remain -= 1;
-    }
-    if (remain === 0) {
-      console.log(`<${answer.join(", ")}>`);
-      return;
-    }
-  }
-}
+console.log(n);
+console.log(numbers);
