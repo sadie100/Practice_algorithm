ef107ffba71e8f58105d47190b82c9ba3142e257
김세희 <sadie1001@naver.com>
2023-06-10 21:15:01 +0900

    06.10 문제풀이

--- test.js
+++ test.js
@@ -5,47 +5,37 @@ const fs = require("fs");
 let [stair, ...scores] = fs
   .readFileSync("input.txt")
   .toString()
+  .trim()
   .split("\n")
   .map(Number);
 
 scores = scores.reverse();
 const memo = new Array(stair).fill(0);
-const visited = new Array(stair).fill(0);
-let ans = 0;
+const visited = new Array(stair).fill(false);
+
+if (stair === 1) {
+  console.log(scores[0]);
+  return;
+}
 
 memo[0] = scores[0];
-visited[0] = 1;
 memo[1] = scores[0] + scores[1];
-visited[1] = 2;
+visited[1] = true;
 
-const queue = [];
-queue.push([[...memo], [...visited], 1]);
-while (queue.length > 0) {
-  const [newMemo, newVisited, nowIdx] = queue.pop();
+if (stair < 3) {
+  console.log(memo[stair - 1]);
+  return;
+}
 
-  for (let i = nowIdx + 1; i < stair; i++) {
-    if (newVisited[i - 2] === 2 && newVisited[i - 1] === 2) {
-      break;
+for (let i = 2; i < stair; i++) {
+  if (!visited[i - 1]) {
+    memo[i] = Math.max(memo[i - 2] + scores[i], memo[i - 1] + scores[i]);
+    if (memo[i - 1] > memo[i - 2]) {
+      visited[i] = true;
     }
-    if (newVisited[i - 2] === 2) {
-      newMemo[i] = newMemo[i - 1] + scores[i];
-      newVisited[i] = newVisited[i - 1] + 1;
-    } else if (newVisited[i - 1] === 2) {
-      newMemo[i] = newMemo[i - 2] + scores[i];
-      newVisited[i] = 1;
-    } else {
-      newMemo[i] = newMemo[i - 2] + scores[i];
-      newVisited[i] = 1;
-      const addMemo = [...newMemo];
-      const addVisited = [...newVisited];
-      addMemo[i] = newMemo[i - 1] + scores[i];
-      addVisited[i] = newVisited[i - 1] + 1;
-      queue.push([[...addMemo], [...addVisited], i]);
-    }
-    if (i === stair - 1) {
-      ans = Math.max(ans, newMemo[stair - 1]);
-    }
+  } else {
+    memo[i] = memo[i - 2] + scores[i];
   }
 }
 
-console.log(ans);
+console.log(Math.max(memo[stair - 1], memo[stair - 2]));
