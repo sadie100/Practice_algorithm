b33abe641a3bded9a2a2e1f5318d733dbc7b7c7c
김세희 <82367039+sadie100@users.noreply.github.com>
2023-06-20 16:32:41 +0900

    06.20 문제풀이

--- 프로그래머스/못품/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/못품/연속 펄스 부분 수열의 합.js
@@ -1,48 +1,22 @@
 function solution(sequence) {
-  const dpNum = sequence.map(() => 0);
-  const dpMultiply = sequence.map(() => []);
+  const dpPlus = sequence.map((d) => 0);
+  const dpMinus = [...dpPlus];
+  let subNum = 1;
 
-  if (sequence[0] < 0) {
-    //음수일 경우
-    dpNum[0] = sequence[0] * -1;
-    dpMultiply[0] = [-1];
-  } else if (sequence[0] > 0) {
-    dpNum[0] = sequence[0];
-    dpMultiply[0] = [1];
-  } else {
-    dpNum[0] = sequence[0];
-    dpMultiply[0] = [-1, 1];
-  }
+  dpPlus[0] = sequence[0];
+  dpMinus[0] = -sequence[0];
+  subNum = -1;
+  let answer = Math.max(dpPlus[0], dpMinus[0]);
 
-  let answer = dpNum[0];
-  for (let idx = 1; idx < sequence.length; idx++) {
-    const num = sequence[idx];
-    let lastMax = Number.MIN_SAFE_INTEGER;
-    let maxMulti = [];
-    dpMultiply[idx - 1].map((subNum) => {
-      const connected = dpNum[idx - 1] + num * subNum * -1;
-      if (connected > lastMax) {
-        lastMax = connected;
-        maxMulti = [subNum * -1];
-      } else if (connected === lastMax) {
-        maxMulti.push(subNum * -1);
-      }
-    });
-    const subNums = [1, -1];
-    subNums.map((subNum) => {
-      const newStart = num * subNum;
-      if (newStart > lastMax) {
-        lastMax = newStart;
-        maxMulti = [subNum];
-      } else if (newStart === lastMax) {
-        if (!maxMulti.includes(subNum)) {
-          maxMulti.push(subNum);
-        }
-      }
-    });
-    answer = Math.max(answer, lastMax);
-    dpNum[idx] = lastMax;
-    dpMultiply[idx] = maxMulti;
+  for (let i = 1; i < sequence.length; i++) {
+    let num = sequence[i];
+    dpPlus[i] = Math.max(dpPlus[i - 1] + subNum * num, subNum * num);
+    dpMinus[i] = Math.max(
+      dpMinus[i - 1] + subNum * num * -1,
+      subNum * num * -1
+    );
+    answer = Math.max(answer, dpPlus[i], dpMinus[i]);
+    subNum *= -1;
   }
 
   return answer;
