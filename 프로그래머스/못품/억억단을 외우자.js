71c2f7b9a84d8fb8bd59cb3317d5d7d496e36317
김세희 <82367039+sadie100@users.noreply.github.com>
2023-07-02 23:49:46 +0900

    07.02 문제풀이

--- 프로그래머스/못품/억억단을 외우자.js
+++ 프로그래머스/못품/억억단을 외우자.js
@@ -1,29 +1,42 @@
 function solution(e, starts) {
-  var answer = [];
+  var answer = starts.map((d) => 0);
   const newStarts = starts.map((st, id) => [st, id]);
   newStarts.sort((a, b) => b[0] - a[0]);
-  const answers = starts.map((d) => 0);
+  // const answers = starts.map((d)=>0)
   console.log(newStarts);
+  let nowMax = 0;
+  let nowMaxCnt = 0;
   // const sqrt = Math.ceil(Math.sqrt(e));
-  // const table = {};
-  let idx = 0;
+  const table = {};
   for (let i = e; i >= 1; i--) {
+    let idx = 0;
+    const tempAnswers = starts.map((d) => 0);
     for (let j = Math.floor(e / i); j >= 1; j--) {
       console.log("i", i, "j", j);
-      // if (i * j > e) break;
+      let number = i * j;
+      if (number > e) break;
+
       let increase = 1;
       if (i !== j) increase = 2;
-      while (j * i < newStarts[idx][0]) {
-        const oldIdx = newStarts[idx][1];
-        idx += 1;
-        const newIdx = newStarts[idx][1];
-        console.log(oldIdx, newIdx);
-        answers[newIdx] = answers[oldIdx];
+
+      if (table[number]) {
+        table[number] += increase;
+      } else {
+        table[number] = increase;
       }
-      const newIdx = newStarts[idx][1];
-      answers[newIdx] += increase;
+
+      if (nowMaxCnt < table[number]) {
+        nowMax = number;
+        nowMaxCnt = table[number];
+      }
+
+      while (number < newStarts[idx][0]) {
+        let answerIdx = newStarts[idx][1];
+        answer[answerIdx] = nowMax;
+        idx += 1;
+      }
     }
   }
 
-  return answers;
+  return answer;
 }
